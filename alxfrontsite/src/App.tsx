adb330a4b478989506d4b0cf54d0243849321c72
Nguyen Vu Hoa <nguyenvuhoa@Nguyens-MacBook-Pro-2.local>
2022-08-17 12:32:36 +0700

    init folder structure

--- alxfrontsite/src/App.tsx
+++ alxfrontsite/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import "./App.scss";
+import BlogPage from "./pages/Blog/BlogPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Blog</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Routes>
+            <Route path="/" element={<BlogPage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ alxfrontsite/src/pages/Blog/BlogPage.tsx
@@ -0,0 +1,5 @@
+function BlogPage() {
+  return <h1>BlogPage works!</h1>;
+}
+
+export default BlogPage;
